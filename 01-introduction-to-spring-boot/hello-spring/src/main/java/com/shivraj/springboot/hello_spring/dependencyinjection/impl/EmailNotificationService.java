79065058911585edd08d82a501a4cf360bbf175d
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-04 23:41:55 +0530

    introduction-to-spring-boot/hello-spring: resolve ambiguity using @Primary annotation if more than one bean of same type present

--- 01-introduction-to-spring-boot/hello-spring/src/main/java/com/shivraj/springboot/hello_spring/dependencyinjection/impl/EmailNotificationService.java
+++ 01-introduction-to-spring-boot/hello-spring/src/main/java/com/shivraj/springboot/hello_spring/dependencyinjection/impl/EmailNotificationService.java
@@ -1,9 +1,11 @@
 package com.shivraj.springboot.hello_spring.dependencyinjection.impl;
 
 import com.shivraj.springboot.hello_spring.dependencyinjection.NotificationService;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Primary
+@Component
 public class EmailNotificationService implements NotificationService {
     @Override
     public void sendNotification(String message) {
