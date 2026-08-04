c188fa3bb5e7f59e2a7166789a8589265765dd3c
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-04 12:15:24 +0530

    introduction-to-spring-boot: create object without bean and try to understand it what it does

--- 01-introduction-to-spring-boot/hello-spring/src/main/java/com/shivraj/springboot/hello_spring/HelloSpringApplication.java
+++ 01-introduction-to-spring-boot/hello-spring/src/main/java/com/shivraj/springboot/hello_spring/HelloSpringApplication.java
@@ -8,6 +8,12 @@ public class HelloSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
+
+        // not a good practice to create an object of PaymentService class like this
+        // because it will not be managed by Spring container
+        // and we will not be able to use the features of Spring framework like dependency injection, AOP, etc.
+        PaymentService paymentService = new PaymentService();
+        paymentService.pay();
 	}
 
 }
---
+++ 01-introduction-to-spring-boot/hello-spring/src/main/java/com/shivraj/springboot/hello_spring/PaymentService.java
@@ -0,0 +1,7 @@
+package com.shivraj.springboot.hello_spring;
+
+public class PaymentService {
+    public void pay() {
+        System.out.println("Paying...");
+    }
+}
