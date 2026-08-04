41a0982a26b2b8114c277b36cebb0c5e72bcbda7
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-04 12:46:13 +0530

    introduction-to-spring-boot/hello-spring: understand the bean lifecycle and simulate it using @PostConstruct and  @PreDestroy annotations

--- 01-introduction-to-spring-boot/hello-spring/src/main/java/com/shivraj/springboot/hello_spring/PaymentService.java
+++ 01-introduction-to-spring-boot/hello-spring/src/main/java/com/shivraj/springboot/hello_spring/PaymentService.java
@@ -1,10 +1,22 @@
 package com.shivraj.springboot.hello_spring;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 //@Component
 public class PaymentService {
     public void pay() {
         System.out.println("Paying...");
+    }
+
+    @PostConstruct
+    public void initXYZ() {
+        System.out.println("PaymentService bean is just initialized");
+    }
+
+    @PreDestroy
+    public void destroyXYZ() {
+        System.out.println("PaymentService bean is about to be destroyed");
     }
 }
