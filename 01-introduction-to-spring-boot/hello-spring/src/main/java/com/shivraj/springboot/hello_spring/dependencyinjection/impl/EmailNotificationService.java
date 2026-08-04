56c3703778c40b8ccf389a64e909b2ab079569d4
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-05 00:33:45 +0530

    introduction-to-spring-boot/hello-spring: use @ConditionalOnProperty annotation to choose the beans depending on the environment variable

--- 01-introduction-to-spring-boot/hello-spring/src/main/java/com/shivraj/springboot/hello_spring/dependencyinjection/impl/EmailNotificationService.java
+++ 01-introduction-to-spring-boot/hello-spring/src/main/java/com/shivraj/springboot/hello_spring/dependencyinjection/impl/EmailNotificationService.java
@@ -2,11 +2,13 @@ package com.shivraj.springboot.hello_spring.dependencyinjection.impl;
 
 import com.shivraj.springboot.hello_spring.dependencyinjection.NotificationService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 //@Primary
 @Qualifier("emailNotificationService")
+@ConditionalOnProperty(name = "notification.type", havingValue = "email", matchIfMissing = true)
 @Component
 public class EmailNotificationService implements NotificationService {
     @Override
--- 01-introduction-to-spring-boot/hello-spring/src/main/java/com/shivraj/springboot/hello_spring/dependencyinjection/impl/SmsNotificationService.java
+++ 01-introduction-to-spring-boot/hello-spring/src/main/java/com/shivraj/springboot/hello_spring/dependencyinjection/impl/SmsNotificationService.java
@@ -2,10 +2,12 @@ package com.shivraj.springboot.hello_spring.dependencyinjection.impl;
 
 import com.shivraj.springboot.hello_spring.dependencyinjection.NotificationService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
 @Qualifier("smsNotificationService")
+@ConditionalOnProperty(name = "notification.type", havingValue = "sms", matchIfMissing = true)
 public class SmsNotificationService implements NotificationService {
     @Override
     public void sendNotification(String message) {
