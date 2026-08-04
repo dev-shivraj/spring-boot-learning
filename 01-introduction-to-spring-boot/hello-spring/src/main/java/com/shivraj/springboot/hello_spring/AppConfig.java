1590207cecd97628ce964cc3904cb10277c595f1
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-04 12:58:11 +0530

    introduction-to-spring-boot/hello-spring: understand the bean scope and simulate it using @Scope annotation

--- 01-introduction-to-spring-boot/hello-spring/src/main/java/com/shivraj/springboot/hello_spring/AppConfig.java
+++ 01-introduction-to-spring-boot/hello-spring/src/main/java/com/shivraj/springboot/hello_spring/AppConfig.java
@@ -2,11 +2,22 @@ package com.shivraj.springboot.hello_spring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean
+    /*
+           default scope is singleton, so we can omit this annotation
+           @Scope("singleton")
+     */
+
+    /*
+              if we want to create a new instance of PaymentService class every time we request it from Spring container
+              then we can use prototype scope
+     */
+    @Scope("prototype")
     PaymentService paymentService() {
         // some more logic to create the bean of PaymentService class
         System.out.println("Creating bean of PaymentService class");
--- 01-introduction-to-spring-boot/hello-spring/src/main/java/com/shivraj/springboot/hello_spring/PaymentService.java
+++ 01-introduction-to-spring-boot/hello-spring/src/main/java/com/shivraj/springboot/hello_spring/PaymentService.java
@@ -16,6 +16,10 @@ public class PaymentService {
     }
 
     @PreDestroy
+    /*
+    when the scope of the bean is singleton, Spring container will manage the lifecycle of the bean, so it will call the destroy method of the bean
+    when the scope of the bean is prototype, Spring container will not manage the lifecycle of the bean, so it will not call the destroy method of the bean
+     */
     public void destroyXYZ() {
         System.out.println("PaymentService bean is about to be destroyed");
     }
