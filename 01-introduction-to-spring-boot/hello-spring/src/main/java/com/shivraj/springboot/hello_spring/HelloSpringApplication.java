8d526dd7a40629022ab849662cd72129d4b42894
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-08-04 12:22:11 +0530

    introduction-to-spring-boot: create a bean with @Component annotation and inject it using @Autowired and try to simulate it by implement the spring boot application by CommandLineRunner interface as we have static main method and we need to autowire

--- 01-introduction-to-spring-boot/hello-spring/src/main/java/com/shivraj/springboot/hello_spring/HelloSpringApplication.java
+++ 01-introduction-to-spring-boot/hello-spring/src/main/java/com/shivraj/springboot/hello_spring/HelloSpringApplication.java
@@ -1,19 +1,31 @@
 package com.shivraj.springboot.hello_spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloSpringApplication {
+public class HelloSpringApplication implements CommandLineRunner {
+
+    @Autowired
+    PaymentService paymentService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
 
-        // not a good practice to create an object of PaymentService class like this
-        // because it will not be managed by Spring container
-        // and we will not be able to use the features of Spring framework like dependency injection, AOP, etc.
-        PaymentService paymentService = new PaymentService();
-        paymentService.pay();
+        /*
+            // not a good practice to create an object of PaymentService class like this
+            // because it will not be managed by Spring container
+            // and we will not be able to use the features of Spring framework like dependency injection, AOP, etc.
+            // PaymentService paymentService = null;
+            PaymentService paymentService = new PaymentService();
+            paymentService.pay();
+         */
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+        paymentService.pay();
+    }
 }
--- 01-introduction-to-spring-boot/hello-spring/src/main/java/com/shivraj/springboot/hello_spring/PaymentService.java
+++ 01-introduction-to-spring-boot/hello-spring/src/main/java/com/shivraj/springboot/hello_spring/PaymentService.java
@@ -1,5 +1,8 @@
 package com.shivraj.springboot.hello_spring;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class PaymentService {
     public void pay() {
         System.out.println("Paying...");
