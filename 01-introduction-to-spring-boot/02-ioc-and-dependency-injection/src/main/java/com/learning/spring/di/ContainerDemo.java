8bc579b0d8272ead5c13e26bbb8ce41e38836d74
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-09-03 00:45:27 +0530

    introduction-to-spring-boot/ioc-and-dependency-injection: register our bean to application context

--- 01-introduction-to-spring-boot/02-ioc-and-dependency-injection/src/main/java/com/learning/spring/di/ContainerDemo.java
+++ 01-introduction-to-spring-boot/02-ioc-and-dependency-injection/src/main/java/com/learning/spring/di/ContainerDemo.java
@@ -5,7 +5,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class ContainerDemo {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext();
-        System.out.println("Application context created");
+        // ApplicationContext context = new AnnotationConfigApplicationContext();
+        //System.out.println("Application context created");
+
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        EmailNotificationService service = new EmailNotificationService();
+
+        context.getBeanFactory().registerSingleton("emailNotificationService", service);
+        context.refresh();
+
+        EmailNotificationService bean = context.getBean(EmailNotificationService.class);
+        bean.send("Hello Spring");
+
     }
 }
---
+++ 01-introduction-to-spring-boot/02-ioc-and-dependency-injection/src/main/java/com/learning/spring/di/EmailNotificationService.java
@@ -0,0 +1,7 @@
+package com.learning.spring.di;
+
+public class EmailNotificationService {
+    public void send(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
