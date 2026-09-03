8804d814b8866b9006b6b4b5f3652f1dd6641c4e
kumar-shivraj <dev.shivraj.kumar@gmail.com>
2026-09-03 22:20:10 +0530

    introduction-to-spring-boot/ioc-and-dependency-injection: collection injection : use @Order annotation for making the priority between multiple implementation of similar beans

--- 01-introduction-to-spring-boot/02-ioc-and-dependency-injection/src/main/java/com/learning/spring/di/collectioninjection/EmailNotificationService.java
+++ 01-introduction-to-spring-boot/02-ioc-and-dependency-injection/src/main/java/com/learning/spring/di/collectioninjection/EmailNotificationService.java
@@ -1,10 +1,12 @@
 package com.learning.spring.di.collectioninjection;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+@Order(2)
 public class EmailNotificationService implements NotificationService {
     @Override
     public void send(String message) {
--- 01-introduction-to-spring-boot/02-ioc-and-dependency-injection/src/main/java/com/learning/spring/di/collectioninjection/SmsNotificationService.java
+++ 01-introduction-to-spring-boot/02-ioc-and-dependency-injection/src/main/java/com/learning/spring/di/collectioninjection/SmsNotificationService.java
@@ -1,10 +1,12 @@
 package com.learning.spring.di.collectioninjection;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 @Qualifier("sms")
+@Order(1)
 public class SmsNotificationService implements NotificationService {
     public void send(String message) {
         System.out.println("Sending sms: " + message);
